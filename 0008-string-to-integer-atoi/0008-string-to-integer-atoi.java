7ffcaf6e23fc0ce73ae203553c27be923ec27d58
Karthik Godugula <gkarthik8461@gmail.com>
2026-07-21 15:12:19 +0530

    0008-STRING-TO-INTEGER-ATOI Stats: Time: 1 ms (100%), Space: 44.1 MB (6.7%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,29 +1,29 @@
 class Solution {
     public int myAtoi(String s) {
-        if( s.length()==0)
+        if(s.length()==0)
             return 0;
-        s =s.trim();
-        if(s.length() == 0)
+
+        s = s.trim();
+        if(s.length()==0)
             return 0;
-        int i = 0 , sign = 1 ;
-        long ans = 0;
-        
-        if(s.charAt(i) == '+' || s.charAt(i) == '-'){
-            if(s.charAt(i) == '-')
+        int sign = 1, i = 0;
+        if(s.charAt(i)=='+' || s.charAt(i)=='-'){
+            if(s.charAt(i)=='-')
                 sign = -1;
             i++;
         }
 
+        long ans = 0;
         while(i<s.length() && Character.isDigit(s.charAt(i))){
-            ans = ans *10 + (s.charAt(i) - '0');
+            ans = ans*10 + s.charAt(i)-'0';
 
-            if(ans*sign > Integer.MAX_VALUE)
+            if(sign*ans>Integer.MAX_VALUE)
                 return Integer.MAX_VALUE;
-            if(ans*sign < Integer.MIN_VALUE)
+            if(sign*ans<Integer.MIN_VALUE)
                 return Integer.MIN_VALUE;
-            
+
             i++;
         }
-        return (int)(ans*sign);
+        return (int)ans*sign;
     }
 }
