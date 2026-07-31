58eae9c824f3ccd909c0ee909a24798eb6b042e3
Karthik Godugula <gkarthik8461@gmail.com>
2026-07-31 21:29:01 +0530

    0050-POWX-N Stats: Time: 0 ms (100%), Space: 47.8 MB (71.75%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,23 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        long N = n;
+       if(N<0){
+            return 1.0/power(x,-N);
+       }
+       return power(x,N);
+    }
+    public double power(double x, long n){
+         // Base case: anything raised to 0 is 1
+        if (n == 0) 
+            return 1.0;
+        
+        // Base case: anything raised to 1 is itself
+        if (n == 1) 
+            return x;
+
+        if(n%2==0)
+            return power(x*x,n/2);
+        
+        return  x*power(x,n-1);
     }
 }
