ffd483bfca5e23e47d75b32fbfd8b9ecf1a432d3
Karthik Godugula <gkarthik8461@gmail.com>
2026-08-03 22:36:33 +0530

    4289-VALID-BINARY-STRINGS-WITH-COST-LIMIT Stats: Time: 9 ms (58.91%), Space: 47.1 MB (96.25%) - LeetHub

--- 4289-valid-binary-strings-with-cost-limit/4289-valid-binary-strings-with-cost-limit.java
+++ 4289-valid-binary-strings-with-cost-limit/4289-valid-binary-strings-with-cost-limit.java
@@ -1,29 +1,27 @@
 class Solution {
     public List<String> generateValidStrings(int n, int k) {
         ArrayList<String> ans = new ArrayList<>();
-        helper(0,0,new StringBuilder(),n,k,ans);
+        helper(0,0,false,new StringBuilder(),n,k,ans);
         return ans;       
     }
-    void helper(int ind,int c , StringBuilder s,int n,int k,ArrayList<String> ans){
+    void helper(int ind,int c , boolean prevOne, StringBuilder s,int n,int k,ArrayList<String> ans){
+        if(c>k)
+            return;
+
         if(ind == n){
-            if(isValid(s.toString()) && c<=k){
-                ans.add(s.toString());
-            }
+            ans.add(s.toString());
             return;
         }
+
         s.append("0");
-        helper(ind+1,c,s,n,k,ans);
+        helper(ind+1,c,false,s,n,k,ans);
         s.deleteCharAt(s.length()-1);
 
-        s.append("1");
-        helper(ind+1,c+ind,s,n,k,ans);
-        s.deleteCharAt(s.length()-1);
-    }
-    boolean isValid(String s){
-        for(int i = 0;i<s.length()-1;i++){
-            if(s.charAt(i) == '1' && s.charAt(i+1)=='1')
-                return false;
+        if(!prevOne){
+            s.append("1");
+            helper(ind+1,c+ind,true,s,n,k,ans);
+            s. deleteCharAt(s.length()-1);
         }
-        return true;
     }
+    
 }
