87fc4280e3a8ed461e0b12a15b72487f87d7496e
Karthik Godugula <gkarthik8461@gmail.com>
2026-08-03 22:21:57 +0530

    4289-VALID-BINARY-STRINGS-WITH-COST-LIMIT Stats: Time: 78 ms (17.42%), Space: 47.4 MB (51.64%) - LeetHub

--- 4289-valid-binary-strings-with-cost-limit/4289-valid-binary-strings-with-cost-limit.java
+++ 4289-valid-binary-strings-with-cost-limit/4289-valid-binary-strings-with-cost-limit.java
@@ -1,22 +1,22 @@
 class Solution {
     public List<String> generateValidStrings(int n, int k) {
-        HashSet<String> set = new HashSet<>();
-        helper(0,0,new StringBuilder(),n,k,set);
-        return new ArrayList<>(set);       
+        ArrayList<String> ans = new ArrayList<>();
+        helper(0,0,new StringBuilder(),n,k,ans);
+        return ans;       
     }
-    void helper(int ind,int c , StringBuilder s,int n,int k,HashSet<String> set){
+    void helper(int ind,int c , StringBuilder s,int n,int k,ArrayList<String> ans){
         if(ind == n){
             if(isValid(s.toString()) && c<=k){
-                set.add(s.toString());
+                ans.add(s.toString());
             }
             return;
         }
         s.append("0");
-        helper(ind+1,c,s,n,k,set);
+        helper(ind+1,c,s,n,k,ans);
         s.deleteCharAt(s.length()-1);
 
         s.append("1");
-        helper(ind+1,c+ind,s,n,k,set);
+        helper(ind+1,c+ind,s,n,k,ans);
         s.deleteCharAt(s.length()-1);
     }
     boolean isValid(String s){
