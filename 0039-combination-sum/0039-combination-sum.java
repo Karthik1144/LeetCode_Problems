9b627984a35c8c41c7f8f65475f5fe134f197e31
Karthik Godugula <gkarthik8461@gmail.com>
2026-09-04 10:35:40 +0530

    0039-COMBINATION-SUM Stats: Time: 5 ms (7.57%), Space: 45.3 MB (98.07%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,20 +1,24 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
-        findCombinations(candidates,0,target,ans,new ArrayList<>());
+        combination(candidates,target,0,0,new ArrayList<>(),ans);
         return ans;
     }
-    public void findCombinations(int []candidates,int ind,int target,          List<List<Integer>> ans,List<Integer> lst){
-        if(ind==candidates.length){
-            if(target==0)
+    public void combination(int[] candidates,int target,int i,int sum,
+                                    List<Integer> lst,List<List<Integer>> ans){
+        if(i==candidates.length){
+            if(sum==target){
                 ans.add(new ArrayList<>(lst));
+            }
             return;
         }
-        if(candidates[ind]<=target){
-            lst.add(candidates[ind]);
-            findCombinations(candidates,ind,target-candidates[ind],ans,lst);
+        if(sum<target){
+            lst.add(candidates[i]);
+            sum += candidates[i];
+            combination(candidates,target,i,sum,lst,ans);
             lst.remove(lst.size()-1);
+            sum -= candidates[i];
         }
-        findCombinations(candidates,ind+1,target,ans,lst);
+        combination(candidates,target,i+1,sum,lst,ans);
     }
 }
